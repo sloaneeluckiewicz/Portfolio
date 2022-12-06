6424b7737bdad77ff3e6e03b84a993b7af3a64b5
Sloane Luckiewicz <sloaneluckiewicz@gmail.com>
2022-12-06 14:23:11 -0500

    Introduction
    One letter at a time introduction.
    setTimeout, useEffect

--- sloane-luckiewicz/src/components/Hello/Hello.js
+++ sloane-luckiewicz/src/components/Hello/Hello.js
@@ -1,23 +1,48 @@
-import React from 'react';
-import styles from './styles.module.css';
+import styles from "./styles.module.css";
+import {React, useState, useEffect} from "react";
 
-class Hello extends React.Component {
-    constructor() {
-      super();
-    }
+function Hello() {
+    const first_text = "Hello!";
+    const second_text = "My name is Sloane Luckiewicz";
+    const third_text = "and this is my portfolio.";
   
-    async componentDidMount() {
-      await new Promise(resolve => setTimeout(resolve, 3000));
-    }
-  
-    render() {
-        return (
-            <div className={styles.main}>
-                <p>Hello my name is Sloane Luckiewicz </p>
-            </div>
-        )
-    }
-  }
+    const [text1, setText1] = useState("");
+    const [text2, setText2] = useState("");
+    const [text3, setText3] = useState("");
 
-  export default Hello ;
+
+    useEffect(() => {
+      const timeout = setTimeout(() => {
+        setText1(first_text.slice(0, text1.length + 1));
+      }, 200);
+      return () => clearTimeout(timeout);
+    }, [text1]);
   
+    useEffect(() => {
+      const timeout = setTimeout(() => {
+        setText2(second_text.slice(0, text2.length + 1));
+      }, 100);
+  
+      return () => clearTimeout(timeout);
+    }, [text2]);
+  
+    useEffect(() => {
+      const timeout = setTimeout(() => {
+        setText3(third_text.slice(0, text3.length + 1));
+      }, 100);
+  
+      return () => clearTimeout(timeout);
+    }, [text3]);
+
+  return (
+    <div className={styles.main}>
+        <span className={styles.message}>
+          <div>{text1}</div>
+          <div>{text2}</div>
+          <div>{text3}</div>
+        </span>
+    </div>
+  );
+}
+
+export default Hello;
