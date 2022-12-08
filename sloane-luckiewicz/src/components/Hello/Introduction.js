025b7eada215b2cd44270f0eb0a7c6dcfd06c734
Sloane Luckiewicz <sloaneluckiewicz@gmail.com>
2022-12-08 18:21:56 -0500

    attempt 2
    trying to have the useHello function go one at a time in Introduction component. Struggling to add useEffect & wait.

--- sloane-luckiewicz/src/components/Hello/Introduction.js
+++ sloane-luckiewicz/src/components/Hello/Introduction.js
@@ -4,15 +4,19 @@ import styles from './styles.module.css';
 import { UseHello } from './useHello';
 import { wait } from '../../Helpers/wait';
 
-export const Introduction = () => {
+export const Introduction = (message) => {
     const [myMessage, setMyMessage] = useState('');
 
     useEffect(()=> {
+        const message = UseHello(message)
         (async () => {
             setMyMessage(`Hello, my name is Sloane Luckiewicz.`);
             await wait(1000);
+
+            setMyMessage(`Welcome to my portfolio.`);
+            await wait(1000);
         })
-    })
+    }, [message])
 
     return {
         myMessage
@@ -21,22 +25,40 @@ export const Introduction = () => {
 }
 */
 
-
 //import { useEffect, useState } from 'react';
 import styles from './styles.module.css';
 import { UseHello } from './useHello';
-//import { wait } from '../../Helpers/wait';
 
-export const Introduction = () => {
+export const Introduction = () => { 
     return (
         <div className={styles.main}>
             <UseHello
-                message = {`Hello, my name is Sloane Luckiewicz!`}
-            /> 
-            <UseHello
-                message = {`Welcome to my portfolio`}
-            /> 
+                message = {'Hello, my name is Sloane Luckiewicz!'}
+            />
         </div>   
     )
-}
-    
+} 
+
+/*
+
+import { useEffect, useState } from 'react';
+import styles from './styles.module.css';
+//import { UseHello } from './useHello';
+import { wait } from '../../Helpers/wait';
+
+export const Introduction = () => {
+    const [myMessage, setMyMessage] = useState('');
+
+     useEffect(() => {
+        setMyMessage('');
+        (async () => {
+            setMyMessage('Hello, my name is Sloane Luckiewicz!')
+            await wait(1000);
+            setMyMessage('Welcome to my portfolio.')
+        })
+     }, [myMessage])
+    return (
+       myMessage  
+    )
+} 
+*/
--- sloane-luckiewicz/src/components/Hello/useHello.js
+++ sloane-luckiewicz/src/components/Hello/useHello.js
@@ -2,7 +2,7 @@ import { Hello } from './Hello';
 
 export const UseHello = ({ message }) => {
     const typedMessage = Hello(message);
-
+    
     return (
         <div>
             <div>{typedMessage}</div>
