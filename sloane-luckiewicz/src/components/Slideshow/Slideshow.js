2ae2d0371d8a5dbcedb187796ee28c5e59bb7e26
Sloane Luckiewicz <sloaneluckiewicz@gmail.com>
2023-02-01 13:16:30 -0500

    slideshow component example

--- sloane-luckiewicz/src/components/Slideshow/Slideshow.js
+++ sloane-luckiewicz/src/components/Slideshow/Slideshow.js
@@ -1,61 +1,40 @@
-import React from "react";
+import React from 'react';
+import { Slide } from 'react-slideshow-image';
 import styles from './styles.module.css';
 
-const pictures = ["#00C49F", "#00C49F", "#FFBB28"];
-const delay = 2500;
 
-export function Slideshow() {
-  const [index, setIndex] = React.useState(0);
-  const timeoutRef = React.useRef(null);
+const slideImages = [
+  'sloane-luckiewicz/src/images/arthur1.PNG',
+  'sloane-luckiewicz/src/images/arthur2.PNG',
+  'sloane-luckiewicz/src/images/ham_bird.jpeg'
+];
 
-  function resetTimeout() {
-    if (timeoutRef.current) {
-      clearTimeout(timeoutRef.current);
-    }
-  }
-
-  React.useEffect(() => {
-    resetTimeout();
-    timeoutRef.current = setTimeout(
-      () =>
-        setIndex((prevIndex) =>
-          prevIndex === pictures.length - 1 ? 0 : prevIndex + 1
-        ),
-      delay
-    );
-
-    return () => {
-      resetTimeout();
-    };
-  }, [index]);
-
-  return (
-    <div className={styles.slideshow}>
-      <div
-        className={styles.slideshowSlider}
-        style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
-      >
-        {pictures.map((backgroundColor, index) => (
-          <div
-            className={styles.slide}
-            key={index}
-            style={{ backgroundColor }}
-          ></div>
-        ))}
-      </div>
-
-      <div className={styles.slideshowDots}>
-        {pictures.map((_, idx) => (
-          <div
-            key={idx}
-            className={styles.slideshowDot}
-            onClick={() => {
-              setIndex(idx);
-            }}
-          ></div>
-        ))}
-      </div>
-    </div>
-  );
+const properties = {
+  duration: 5000,
+  transitionDuration: 500,
+  infinite: true,
+  indicators: true,
+  arrows: true
 }
 
+export const Slideshow = () => {
+    return (
+      <Slide {...properties}>
+        <div className={styles.eachSlide}>
+          <div style={{'backgroundImage': `url(${slideImages[0]})`}}>
+            <span>Slide 1</span>
+          </div>
+        </div>
+        <div className={styles.eachSlide}>
+          <div style={{'backgroundImage': `url(${slideImages[1]})`}}>
+            <span>Slide 2</span>
+          </div>
+        </div>
+        <div className={styles.eachSlide}>
+          <div style={{'backgroundImage': `url(${slideImages[2]})`}}>
+            <span>Slide 3</span>
+          </div>
+        </div>
+      </Slide>
+    )
+}
